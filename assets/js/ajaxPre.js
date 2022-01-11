2005a35e15299e1a09f1a4a317feead2b4e2d110
jx <1417428201@qq.com>
2022-01-11 20:20:07 +0800

    完成了注册和登录功能的开发

--- assets/js/ajaxPre.js
+++ assets/js/ajaxPre.js
@@ -12,5 +12,5 @@ $.ajaxPrefilter(function(options) {
         }
     }
 
-    options.url = 'http://127.0.0.1:3007' + options.url
+    options.url = 'http://www.liulongbin.top:3007' + options.url
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,63 +1,76 @@
-$(function() {
-    $('#link_login').on('click', function() {
-        $('.login-box').hide()
-        $('.reg-box').show()
-    })
+$(function () {
+	//点击去注册链接
+	$('#link_reg').on('click', function () {
+		$('.login-box').hide();
+		$('.reg-box').show();
+	})
 
-    $('#link_reg').on('click', function() {
-        $('.reg-box').hide()
-        $('.login-box').show()
-    })
+	//点击去登录
+	$('#link_login').on('click', function () {
+		$('.login-box').show();
+		$('.reg-box').hide();
+	})
 
-    var form = layui.form
-    var layer = layui.layer
+	//从 layui 中获取 form 对象
+	var form = layui.form
+	var layer = layui.layer
+	//通过 form.verify() 函数自定义 校验规则
+	form.verify({
+		//自定义了一个叫pwd的校验规则
+		pwd:
+			[/^[\S]{6,12}$/,
+				'密码必须6到12位，且不能出现空格'
+			],
+		//校验两次密码是否一样的规则
+		repwd: function (value) {
+			//通过形参拿到的是确定密码的值
+			//还需要拿到密码框的值
+			//进行一次比较
+			//如果判断失败，return一个失败的消息就行
+			var pwd = $('.reg-box [name=password]').val()
+			if (pwd !== value) {
+				return '两次密码不一致！'
+			}
+		}
+	})
 
-    form.verify({
-        pass: [
-            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
-        ],
+	//监听注册表单的提交事件
+	$('#form_reg').on('submit', function (e) {
+		//阻止默认行为
+		e.preventDefault();
+		var data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }
+		$.post('/api/reguser', data, function (res) {
+			if (res.status !== 0) {
+				return layer.msg(res.message)
+			}
+			layer.msg('注册成功,请登录!')
+			//模拟人的点击行为
+			$('#link_login').click()
+		})
+	})
 
-        repass: function(newpwd) {
-            var oldpwd = $('.reg-box [name=password]').val()
+	//监听登录表单的提交事件
+	$('#form_login').submit(function (e) {
+		//阻止默认提交行为
 
-            if (newpwd != oldpwd) {
-                return '两次密码不一致'
-            }
-        }
-    })
+		e.preventDefault();
+		$.ajax({
+			url: '/api/login',
+			method: 'POST',
+			//快速获取表单中的数据
+			data: $(this).serialize(),
+			success: function (res) {
+				if (res.status !== 0) {
+					return layer.msg('登录失败！')
+				}
+				layer.msg('登录成功！')
+				//将登录成功的 token 存到localStorage 中
+				// console.log(res.token)
+				localStorage.setItem('token',res.token)
 
-    $('#form-reg').on('submit', function(e) {
-        e.preventDefault()
-
-        $.ajax({
-            url: '/api/reguser',
-            type: 'POST',
-            data: $(this).serialize(),
-            success: function(res) {
-                if (res.status !== 0) {
-                    return layer.msg(res.msg)
-                }
-                layer.msg('注册成功')
-                $('#link_reg').trigger('click')
-            }
-        })
-    })
-
-    $('#form-login').on('submit', function(e) {
-        e.preventDefault()
-
-        $.ajax({
-            url: '/api/login',
-            type: 'POST',
-            data: $(this).serialize(),
-            success: function(res) {
-                if (res.status !== 0) {
-                    return layer.msg(res.msg)
-                }
-                layer.msg('登录成功')
-                localStorage.setItem('token', res.token)
-                location.href = './index.html'
-            }
-        })
-    })
+				//跳转到后台
+				location.href='/index.html'
+			}
+		})
+	})
 })

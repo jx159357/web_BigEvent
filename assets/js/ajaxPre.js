ee92339b69dfb6991df5ba1b11eac2fb76064768
jx <1417428201@qq.com>
2022-01-13 19:29:29 +0800

    完成了主页功能的开发

--- assets/js/ajaxPre.js
+++ assets/js/ajaxPre.js
@@ -1,16 +1,25 @@
 // 每次发起$.ajax或$.post或$.get请求之前，都会先调用$.ajaxPrefilter
 // $.ajaxPrefilter的options 参数时 $.ajax或$.post或$.get的对象
-$.ajaxPrefilter(function(options) {
-    if (options.url.startsWith('/my')) {
-        options.headers = { Authorization: localStorage.getItem('token') || '' }
+$.ajaxPrefilter(function (options) {
 
-        options.complete = function(res) {
-            if (res.responseJSON.status === 1 && res.responseJSON.msg === '身份认证失败') {
-                localStorage.removeItem('token')
-                location.href = '/login.html'
-            }
+    //统一为有权限的接口 设置headers 请求头
+    // if (options.url.startsWith('/my')) 
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
         }
     }
 
+    //全局统一挂载complete 回调函数
+    options.complete = function (res) {
+        console.log(res)
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.强制清空token
+            localStorage.removeItem('token')
+            //2. 强制跳转到 登录页面
+            location.href = '/login.html'
+        }
+    }
+    //在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,58 @@
-$(function() {
+$(function () {
+    // 调用getUserInfo 获取用户基本信息
     getUserInfo()
 
-    $('#btnLogout').on('click', function() {
+    $('#btnLogout').on('click', function () {
+        //提示用户是否退出
         layui.layer.confirm('确定要退出吗？', {
             icon: 3,
             title: '提示',
-            btn: ['确定', '取消']
-        }, function(index) {
+            btn: ['确定了', '取消吧']
+        }, function (index) {
+            //1.清空本地存储的token
             localStorage.removeItem('token')
+            //2.重新跳转到首页
             location.href = '/login.html'
+            //关闭 comfirm 询问框
             layui.layer.close(index)
         });
     })
 })
 
+//获取用户的基本信息
 function getUserInfo() {
     $('.layui-nav-img').hide()
     $.ajax({
         url: '/my/userinfo',
         method: 'GET',
-        success: function(res) {
+        success: function (res) {
             if (res.status !== 0) {
-                return layui.layer.msg(res.msg)
+                return layui.layer.msg(res.message)
             }
+            //调用渲染用户的头像
             renderAvatar(res.data)
         }
     })
 }
 
+//渲染用户的头像
 function renderAvatar(user) {
-    // 欢迎用户
+    // 1.获取用户名称
     var name = user.nickname || user.username
-    $('#welcome').html(`欢迎 ${name}`)
+    //2.设置欢迎的 文本
+    // $('#welcome').html(`欢迎 ${name}`)
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
 
-    // 用户头像渲染
+    //3. 按需渲染用户头像
     if (user.user_pic) {
+        //3.1渲染图片头像
         $('.layui-nav-img').attr('src', user.user_pic).show()
         $('.text-avatar').hide()
     } else {
+        //3.2渲染文字头像
+        $('.layui-nav-img').hide()
         let first = name[0].toUpperCase()
         $('.text-avatar').html(first).show()
-        $('.layui-nav-img').hide()
     }
 }
 
